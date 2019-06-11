7c0cb8170f670c9be822ee73116b37f25f5f856a
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-11 00:35:42 -0400

    add LCM

--- ex6_17.py
+++ ex6_17.py
@@ -6,7 +6,8 @@ def main():
     common_divisor = min(a, b)
     while not (a % common_divisor == 0 and b % common_divisor == 0):
         common_divisor-=1
-    print('common divisor {}'.format(common_divisor))
+    print('GCD({} , {}) = {}'.format(a, b, common_divisor))
+    print('LCM({} , {}) = {}'.format(a, b, int(a * b / common_divisor)))
 
 if __name__ == "__main__":
     main()
