931f32ee305ec886534713b73c9ba66b14f711b9
Arden Zhu <arden.zhu@lafayette148.com>
2019-06-11 00:37:23 -0400

    add function gcd

--- ex6_17.py
+++ ex6_17.py
@@ -3,11 +3,14 @@ def main():
     a = int(input())
     print('b =', end=' ')
     b = int(input())
+    print('GCD({} , {}) = {}'.format(a, b, gcd(a, b)))
+    print('LCM({} , {}) = {}'.format(a, b, int(a * b / gcd(a, b))))
+
+def gcd(a, b):
     common_divisor = min(a, b)
     while not (a % common_divisor == 0 and b % common_divisor == 0):
         common_divisor-=1
-    print('GCD({} , {}) = {}'.format(a, b, common_divisor))
-    print('LCM({} , {}) = {}'.format(a, b, int(a * b / common_divisor)))
+    return common_divisor
 
 if __name__ == "__main__":
     main()
